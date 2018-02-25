1865985d410d35bdd08f0b00f8c87ede9bc79406
Robert Wu <wu.rupert@outlook.com>
2018-02-25 06:43:57 -0500

    0.1
    Set up main and map activities with location and Google Maps API.

---
+++ app/src/main/java/com/veloce/veloce/CurrentTripsActivity.java
@@ -0,0 +1,92 @@
+package com.veloce.veloce;
+
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.content.ContextCompat;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class CurrentTripsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    public static final int REQUEST_LOCATION_PERMISSION = 99;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_current_trips);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    private void setMapLongClick(final GoogleMap map) {
+        map.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
+            @Override
+            public void onMapLongClick(LatLng latLng) {
+
+            }
+        });
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng torontoEastHarbour = new LatLng(43.656494, -79.345338);
+        mMap.addMarker(new MarkerOptions().position(torontoEastHarbour).title("Marker in Toronto East Harbour"));
+        LatLng montreal = new LatLng(45.499983, -73.566643);
+        mMap.addMarker(new MarkerOptions().position(montreal).title("Marker in Montreal : dest"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(torontoEastHarbour));
+        enableMyLocation();
+    }
+
+    private void enableMyLocation() {
+        if (ContextCompat.checkSelfPermission(this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            mMap.setMyLocationEnabled(true);
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]
+                            {android.Manifest.permission.ACCESS_FINE_LOCATION},
+                    REQUEST_LOCATION_PERMISSION);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String[] permissions,
+                                           int[] grantResults) {
+        // Check if location permissions are granted and if so enable the
+        // location data layer.
+        switch (requestCode) {
+            case REQUEST_LOCATION_PERMISSION:
+                if (grantResults.length > 0
+                        && grantResults[0]
+                        == PackageManager.PERMISSION_GRANTED) {
+                    enableMyLocation();
+                    break;
+                }
+        }
+    }
+
+}
---
+++ app/src/main/java/com/veloce/veloce/MainActivity.java
@@ -0,0 +1,133 @@
+package com.veloce.veloce;
+
+import android.Manifest;
+import android.app.Activity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+
+import android.os.AsyncTask;
+
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.design.widget.NavigationView;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.TextView;
+import android.widget.Toast;
+// used for rest api call
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import android.support.v7.widget.Toolbar;
+
+
+public class MainActivity extends AppCompatActivity{
+
+    private Button mBtLaunchActivity;
+    private Button menuDrawer;
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mDrawerToggle;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar myToolbar = findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+
+        mBtLaunchActivity = (Button) findViewById(R.id.bt_launch_activity);
+        mBtLaunchActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launchProfile();
+            }
+        });
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        mDrawerToggle = new ActionBarDrawerToggle(
+                this,
+                mDrawerLayout,
+                myToolbar,
+                R.string.drawer_open,
+                R.string.drawer_close) {
+        };
+
+        // assign the new drawer toggle to the drawer layout
+        mDrawerLayout.addDrawerListener(mDrawerToggle);
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.left_nav);
+        if (navigationView.equals(null)) {
+            Toast.makeText(this, "fakes", Toast.LENGTH_SHORT).show();
+        } else {
+            navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        // set item as selected to persist highlight
+                        menuItem.setChecked(true);
+                        // close drawer when item is tapped
+                        mDrawerLayout.closeDrawers();
+
+                        // Add code here to update the UI based on the item selected
+                        // For example, swap UI fragments here
+
+                        switch (menuItem.getItemId()) {
+                            case R.id.nav_profile: launchProfile(); break;
+                            case R.id.nav_current_trip: launchCurrentTrip(); break;
+                            default: break;
+
+                        }
+
+                        return true;
+                    }
+                });
+        }
+    }
+
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+
+        //
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+
+        mDrawerToggle.syncState();
+
+    }
+
+    private void launchProfile() {
+
+        Intent intent = new Intent(this, ProfileActivity.class);
+        startActivity(intent);
+    }
+    private void launchCurrentTrip(){
+        Intent intent = new Intent(this, CurrentTripsActivity.class);
+        startActivity(intent);
+    }
+
+    public void submitBtn(View v) {
+
+    }
+
+    public void openNavProfile() {
+        Toast.makeText(this, "Profile Opened", Toast.LENGTH_SHORT);
+    }
+
+}
