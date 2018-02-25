22a4cf03b8a1f3d6cececa191d32f3288a0a1f8c
Robert Wu <wu.rupert@outlook.com>
2018-02-25 09:48:51 -0500

    0.4.1

--- veloce_json_parser.java
+++ veloce_json_parser.java
@@ -3,8 +3,9 @@ public class JSONParser{
         jsonStr = jsonStr.substring(jsonStr.indexOf('['))
         jsonStr = jsonStr.substring(jsonStr.indexOf('['))
         jsonStr = jsonStr.substring(0, jsonStr.indexOf(']'))
-
+        c1 = StringUtils.countMatches("},{")
         jsonStr = jsonStr.split("},{");
+        String[] rides = new String[c1]
         for (int i=0; i<jsonStr.length(); i++){
             String[] items = jsonStr[i].split(",");
             for (int j=0; j<items.length(); j++){
@@ -13,6 +14,6 @@ public class JSONParser{
             }
             jsonStr[i] = items
         }
-        return jsonStr
+        return rides
     }
 }
